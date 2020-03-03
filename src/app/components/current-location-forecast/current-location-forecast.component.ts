b1210b147ebebd269c524bbb78d2beea827bf5f1
Jaime Noel Alvarez Luna <jaimealv994@gmail.com>
2020-03-03 06:33:19 -0600

    added current-location-forecast and current-location-weather components

---
+++ src/app/components/current-location-forecast/current-location-forecast.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-current-location-forecast',
+  templateUrl: './current-location-forecast.component.html',
+  styleUrls: ['./current-location-forecast.component.scss'],
+})
+export class CurrentLocationForecastComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ src/app/components/current-location-weather/current-location-weather.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-current-location-weather',
+  templateUrl: './current-location-weather.component.html',
+  styleUrls: ['./current-location-weather.component.scss'],
+})
+export class CurrentLocationWeatherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
